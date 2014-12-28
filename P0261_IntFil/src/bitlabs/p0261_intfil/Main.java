ebe3b7ed377ac561d78b36c7a9d013afa2ae8e91
bit <it_nv@mail.ru>
2014-12-28 19:17:18 +0300

    jj

--- P0261_IntFil/src/bitlabs/p0261_intfil/Main.java
+++ P0261_IntFil/src/bitlabs/p0261_intfil/Main.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Main extends Activity{
+public class Main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
